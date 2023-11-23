691299fb725a4ba19c63bff2f3473709a910359e
alpha <alphamodeyt@gmail.com>
2023-11-22 20:28:46 -0600

    chore: switch to entity refactor and only use modules we need

--- src/main/java/com/aetherteam/nitrogen/integration/rei/FluidStateRenderer.java
+++ src/main/java/com/aetherteam/nitrogen/integration/rei/FluidStateRenderer.java
@@ -7,7 +7,7 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.math.Axis;
-import io.github.fabricators_of_create.porting_lib.util.FluidStack;
+import io.github.fabricators_of_create.porting_lib.fluids.FluidStack;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.entry.renderer.EntryRenderer;
 import me.shedaniel.rei.api.client.gui.widgets.Tooltip;
