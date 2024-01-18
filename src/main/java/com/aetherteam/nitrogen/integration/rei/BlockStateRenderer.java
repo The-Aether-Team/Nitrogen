684bd4ad111b0645d77fa98b508dab28669f010c
Blodhgarm <timekeeperguild@gmail.com>
2024-01-17 18:04:30 -0600

    feat: 1.20.1 REI Implementation (#20)

---
+++ src/main/java/com/aetherteam/nitrogen/integration/rei/BlockStateRenderer.java
@@ -0,0 +1,145 @@
+package com.aetherteam.nitrogen.integration.rei;
+
+import com.aetherteam.nitrogen.Nitrogen;
+import com.aetherteam.nitrogen.integration.jei.FakeLevel;
+import com.aetherteam.nitrogen.recipe.BlockPropertyPair;
+import com.aetherteam.nitrogen.recipe.BlockStateRecipeUtil;
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.entry.renderer.EntryRenderer;
+import me.shedaniel.rei.api.client.gui.widgets.Tooltip;
+import me.shedaniel.rei.api.client.gui.widgets.TooltipContext;
+import me.shedaniel.rei.api.common.entry.EntryStack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.ModelBlockRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import org.joml.Vector3f;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public record BlockStateRenderer(BlockPropertyPair... pairs) implements EntryRenderer<ItemStack> {
+    @Override
+    public void render(EntryStack<ItemStack> entry, GuiGraphics guiGraphics, Rectangle bounds, int mouseX, int mouseY, float delta) {
+        PoseStack poseStack = guiGraphics.pose();
+
+        Minecraft minecraft = Minecraft.getInstance();
+        BlockRenderDispatcher blockRenderDispatcher = minecraft.getBlockRenderer();
+
+        BlockPropertyPair pair = this.getMatchingPair(entry.getValue());
+
+        poseStack.pushPose();
+        poseStack.translate(bounds.x, bounds.y, 0);
+
+        if (pair.block() != null && pair.properties() != null && minecraft.level != null) {
+            BlockState blockState = pair.block().defaultBlockState();
+            for (Map.Entry<Property<?>, Comparable<?>> propertyEntry : pair.properties().entrySet()) {
+                blockState = BlockStateRecipeUtil.setHelper(propertyEntry, blockState);
+            }
+
+            poseStack.pushPose();
+
+            poseStack.translate(15.0F, 12.33F, 5.0F);
+            poseStack.mulPose(Axis.XP.rotationDegrees(-30.0F));
+            poseStack.mulPose(Axis.YP.rotationDegrees(45.0F));
+            poseStack.scale(-9.9F, -9.9F, -9.9F);
+
+            RenderSystem.setupGui3DDiffuseLighting((new Vector3f(0.4F, 0.0F, 1.0F)).normalize(), (new Vector3f(-0.4F, 1.0F, -0.2F)).normalize());
+
+            ModelBlockRenderer modelBlockRenderer = blockRenderDispatcher.getModelRenderer();
+            MultiBufferSource.BufferSource bufferSource = minecraft.renderBuffers().bufferSource();
+            BakedModel model = blockRenderDispatcher.getBlockModel(blockState);
+            modelBlockRenderer.tesselateBlock(FakeLevel.of(blockState), model, blockState, BlockPos.ZERO, poseStack, bufferSource.getBuffer(Sheets.translucentCullBlockSheet()), false, minecraft.level.getRandom(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY);
+            bufferSource.endBatch();
+
+            Lighting.setupFor3DItems();
+
+            poseStack.popPose();
+        }
+
+        poseStack.popPose();
+    }
+
+    @Override
+    public Tooltip getTooltip(EntryStack<ItemStack> ingredient, TooltipContext context) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Player player = minecraft.player;
+        try {
+            Tooltip tooltip = Tooltip.create();
+
+            BlockPropertyPair pair = this.getMatchingPair(ingredient.getValue());
+            Block block = pair.block();
+            Map<Property<?>, Comparable<?>> properties = pair.properties();
+
+            if (block != null && properties != null) {
+                // Display block name.
+                MutableComponent mutablecomponent = Component.empty().append(block.getName()).withStyle(ingredient.getValue().getRarity().color);
+                tooltip.add(mutablecomponent);
+                if (context.getFlag().isAdvanced()) {
+                    ResourceLocation blockKey = BuiltInRegistries.BLOCK.getKey(block);
+                    if (block.defaultBlockState().isAir()) {
+                        tooltip.add(Component.literal(blockKey.toString()).withStyle(ChatFormatting.DARK_GRAY));
+                    }
+                }
+                // Display whether this blockstate is enabled.
+                if (player != null && !ingredient.getValue().getItem().isEnabled(player.level().enabledFeatures())) {
+                    tooltip.add(Component.translatable("item.disabled").withStyle(ChatFormatting.RED));
+                }
+                // Display block properties.
+                if (!properties.isEmpty()) {
+                    tooltip.add(Component.translatable("gui.aether.jei.properties.tooltip").withStyle(ChatFormatting.GRAY));
+                    for (Map.Entry<Property<?>, Comparable<?>> entry : properties.entrySet()) {
+                        tooltip.add(Component.literal(entry.getKey().getName() + ": " + entry.getValue().toString()).withStyle(ChatFormatting.DARK_GRAY));
+                    }
+                }
+            }
+            return tooltip;
+        } catch (RuntimeException | LinkageError e) {
+            Nitrogen.LOGGER.error("Failed to get tooltip: {}", ingredient, e);
+            return Tooltip.create(Component.translatable("jei.tooltip.error.crash").withStyle(ChatFormatting.RED));
+        }
+    }
+
+    /**
+     * Warning for "deprecation" is suppressed because the non-sensitive version of {@link net.minecraft.world.level.block.Block#getCloneItemStack(BlockGetter, BlockPos, BlockState)} is needed in this context.
+     */
+    private BlockPropertyPair getMatchingPair(ItemStack ingredient) {
+        Map<Block, Map<Property<?>, Comparable<?>>> pairsMap = Stream.of(this.pairs).collect(Collectors.toMap(BlockPropertyPair::block, BlockPropertyPair::properties));
+        Block block = null;
+        Map<Property<?>, Comparable<?>> propertiesMap = null;
+        if (Minecraft.getInstance().level != null) {
+            for (Map.Entry<Block, Map<Property<?>, Comparable<?>>> entry : pairsMap.entrySet()) {
+                ItemStack stack = entry.getKey().getCloneItemStack(Minecraft.getInstance().level, BlockPos.ZERO, entry.getKey().defaultBlockState());
+                stack = stack.isEmpty() ? new ItemStack(Blocks.STONE) : stack;
+                if (stack.getItem() == ingredient.getItem()) {
+                    block = entry.getKey();
+                    propertiesMap = entry.getValue();
+                }
+            }
+        }
+        return BlockPropertyPair.of(block, propertiesMap);
+    }
+}
