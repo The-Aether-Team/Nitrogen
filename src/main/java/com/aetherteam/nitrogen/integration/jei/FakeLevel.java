c2b67c151882697fd92f1c6af720d92d4a0e3103
bconlon <bconlon.gml@gmail.com>
2024-01-19 11:37:00 -0800

    feat: 1.19.4 REI Implementation

--- src/main/java/com/aetherteam/nitrogen/integration/jei/FakeLevel.java
+++ src/main/java/com/aetherteam/nitrogen/integration/jei/FakeLevel.java
@@ -4,13 +4,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.Holder;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.ColorResolver;
 import net.minecraft.world.level.LightLayer;
-import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.lighting.LevelLightEngine;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 
 /**
  * A fake level used for rendering.
@@ -39,12 +41,7 @@ public abstract class FakeLevel implements BlockAndTintGetter {
     @Override
     public int getBlockTint(BlockPos pos, ColorResolver colorResolver) {
         ClientLevel level = Minecraft.getInstance().level;
-        if (level != null) {
-            Holder<Biome> biome = Minecraft.getInstance().level.getBiome(pos);
-            return colorResolver.getColor(biome.value(), 0, 0);
-        } else {
-            return -1;
-        }
+        return level != null ? colorResolver.getColor(level.getBiome(pos).value(), 0, 0) : -1;
     }
 
     @Override
@@ -60,5 +57,33 @@ public abstract class FakeLevel implements BlockAndTintGetter {
     @Override
     public int getMinBuildHeight() {
         return 0;
+    }
+
+    public static FakeLevel of(FluidState fluidState){
+        return new FakeLevel() {
+            @Override
+            public BlockState getBlockState(BlockPos pos) {
+                return pos.equals(BlockPos.ZERO) ? fluidState.createLegacyBlock() : Blocks.AIR.defaultBlockState();
+            }
+
+            @Override
+            public FluidState getFluidState(BlockPos pos) {
+                return pos.equals(BlockPos.ZERO) ? fluidState : Fluids.EMPTY.defaultFluidState();
+            }
+        };
+    }
+
+    public static FakeLevel of(BlockState blockState){
+        return new FakeLevel() {
+            @Override
+            public BlockState getBlockState(BlockPos pos) {
+                return pos.equals(BlockPos.ZERO) ? blockState : Blocks.AIR.defaultBlockState();
+            }
+
+            @Override
+            public FluidState getFluidState(BlockPos pos) {
+                return Fluids.EMPTY.defaultFluidState();
+            }
+        };
     }
 }
---
+++ src/main/java/com/aetherteam/nitrogen/integration/rei/displays/FuelDisplay.java
@@ -0,0 +1,36 @@
+package com.aetherteam.nitrogen.integration.rei.displays;
+
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Collection;
+import java.util.List;
+
+public class FuelDisplay extends BasicDisplay {
+    private final CategoryIdentifier<FuelDisplay> id;
+    private final int burnTime;
+    private final Block usage;
+
+    public FuelDisplay(CategoryIdentifier<FuelDisplay> id, Collection<ItemStack> input, int burnTime, Block usage) {
+        super(List.of(EntryIngredients.ofItemStacks(input)), List.of());
+        this.id = id;
+        this.burnTime = burnTime;
+        this.usage = usage;
+    }
+
+    public int getBurnTime() {
+        return this.burnTime;
+    }
+
+    public Block getUsage() {
+        return this.usage;
+    }
+
+    @Override
+    public CategoryIdentifier<FuelDisplay> getCategoryIdentifier() {
+        return this.id;
+    }
+}
