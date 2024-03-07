af58843c34db012e0aff66808b44c4a8599a1544
bconlon <bconlon.gml@gmail.com>
2024-03-06 21:23:24 -0800

    improv: move datagen function

---
+++ src/main/java/com/aetherteam/nitrogen/data/NitrogenDataGenerators.java
@@ -0,0 +1,29 @@
+package com.aetherteam.nitrogen.data;
+
+import com.aetherteam.nitrogen.data.generators.NitrogenLanguageData;
+import net.minecraft.SharedConstants;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraftforge.data.event.GatherDataEvent;
+
+import java.util.Map;
+
+public class NitrogenDataGenerators {
+    public static void onInitializeDataGenerator(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+
+        // Client Data
+        generator.addProvider(event.includeClient(), new NitrogenLanguageData(packOutput));
+
+        // pack.mcmeta
+        PackMetadataGenerator packMeta = new PackMetadataGenerator(packOutput);
+        Map<PackType, Integer> packTypes = Map.of(PackType.SERVER_DATA, SharedConstants.getCurrentVersion().getPackVersion(PackType.SERVER_DATA));
+        packMeta.add(PackMetadataSection.TYPE, new PackMetadataSection(Component.translatable("pack.nitrogen_internals.mod.description"), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES), packTypes));
+        generator.addProvider(true, packMeta);
+    }
+}
