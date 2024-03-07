6cce7c6b3fc993e34b3bbd1f8ff32d28e1501b2c
bconlon <bconlon.gml@gmail.com>
2024-03-06 22:00:56 -0800

    feat: effect desc datagen function

--- src/main/java/com/aetherteam/nitrogen/data/providers/NitrogenLanguageProvider.java
+++ src/main/java/com/aetherteam/nitrogen/data/providers/NitrogenLanguageProvider.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -28,6 +29,10 @@ public abstract class NitrogenLanguageProvider extends LanguageProvider {
 
     public void addTrim(String key, String name) {
         this.add("trim_material." + this.id + "." + key, name + " Material");
+    }
+
+    public void addEffectDesc(Supplier<? extends MobEffect> key, String name) {
+        this.add("effect." + this.id + "." + key.get().getDescriptionId() + ".description", name);
     }
 
     public void addDimension(ResourceKey<Level> dimension, String name) {
