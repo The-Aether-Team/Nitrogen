b04a9403c223c8055739724a8f1944d0b91fbaec
bconlon <bconlon.gml@gmail.com>
2024-04-19 22:53:07 -0700

    feat: AddEntityDropsModifier

--- src/main/java/com/aetherteam/nitrogen/loot/modifiers/AddDungeonLootModifier.java
+++ src/main/java/com/aetherteam/nitrogen/loot/modifiers/AddDungeonLootModifier.java
@@ -1,4 +1,4 @@
-package com.aetherteam.nitrogen.loot;
+package com.aetherteam.nitrogen.loot.modifiers;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
--- src/main/java/com/aetherteam/nitrogen/loot/modifiers/NitrogenLootModifiers.java
+++ src/main/java/com/aetherteam/nitrogen/loot/modifiers/NitrogenLootModifiers.java
@@ -1,4 +1,4 @@
-package com.aetherteam.nitrogen.loot;
+package com.aetherteam.nitrogen.loot.modifiers;
 
 import com.aetherteam.nitrogen.Nitrogen;
 import com.mojang.serialization.Codec;
@@ -11,4 +11,5 @@ public class NitrogenLootModifiers {
     public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> GLOBAL_LOOT_MODIFIERS = DeferredRegister.create(NeoForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, Nitrogen.MODID);
 
     public static final DeferredHolder<Codec<? extends IGlobalLootModifier>, Codec<AddDungeonLootModifier>> ADD_DUNGEON_LOOT = GLOBAL_LOOT_MODIFIERS.register("add_dungeon_loot", () -> AddDungeonLootModifier.CODEC);
+    public static final DeferredHolder<Codec<? extends IGlobalLootModifier>, Codec<AddEntityDropsModifier>> ADD_ENTITY_DROPS = GLOBAL_LOOT_MODIFIERS.register("add_entity_drops", () -> AddEntityDropsModifier.CODEC);
 }
