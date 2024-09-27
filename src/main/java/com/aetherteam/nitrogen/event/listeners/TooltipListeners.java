269b19f885044beae282f84753468e4556a261cc
bconlon <bconlon.gml@gmail.com>
2024-09-27 12:56:53 -0700

    improv: conditional ability tooltips

--- src/main/java/com/aetherteam/nitrogen/event/listeners/TooltipListeners.java
+++ src/main/java/com/aetherteam/nitrogen/event/listeners/TooltipListeners.java
@@ -8,23 +8,35 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @EventBusSubscriber(modid = Nitrogen.MODID)
 public class TooltipListeners {
+    public static Map<ItemStack, TooltipPredicate> PREDICATES = new HashMap<>();
+
     @SubscribeEvent
     public static void onTooltipCreationLowPriority(ItemTooltipEvent event) {
         ItemStack itemStack = event.getItemStack();
         List<Component> itemTooltips = event.getToolTip();
         addAbilityTooltips(itemStack, itemTooltips);
     }
-    
+
     public static void addAbilityTooltips(ItemStack stack, List<Component> components) {
         for (int i = 1; i <= 5; i++) {
             String string = stack.getDescriptionId() + "." + Nitrogen.MODID + ".ability.tooltip." + i;
+            if (PREDICATES.containsKey(stack)) {
+                string = string + "." + PREDICATES.get(stack).override(stack, components);
+            }
             if (I18n.exists(string)) {
                 components.add(i, Component.translatable(string));
             }
         }
+    }
+    
+    @FunctionalInterface
+    public interface TooltipPredicate {
+        String override(ItemStack itemStack, List<Component> components);
     }
 }
