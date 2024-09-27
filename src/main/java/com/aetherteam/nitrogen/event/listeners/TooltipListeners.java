83541957bef2c09d3fff0479f7c5a958e841bffd
bconlon <bconlon.gml@gmail.com>
2024-09-27 13:44:45 -0700

    improv: more predicate context

--- src/main/java/com/aetherteam/nitrogen/event/listeners/TooltipListeners.java
+++ src/main/java/com/aetherteam/nitrogen/event/listeners/TooltipListeners.java
@@ -4,6 +4,7 @@ import com.aetherteam.nitrogen.Nitrogen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -20,18 +21,20 @@ public class TooltipListeners {
 
     @SubscribeEvent
     public static void onTooltipCreationLowPriority(ItemTooltipEvent event) {
+        Player player = event.getEntity();
         ItemStack itemStack = event.getItemStack();
         List<Component> itemTooltips = event.getToolTip();
-        addAbilityTooltips(itemStack, itemTooltips);
+        Item.TooltipContext context = event.getContext();
+        addAbilityTooltips(player, itemStack, itemTooltips, context);
     }
 
-    public static void addAbilityTooltips(ItemStack stack, List<Component> components) {
+    public static void addAbilityTooltips(Player player, ItemStack stack, List<Component> components, Item.TooltipContext context) {
         for (int i = 1; i <= 5; i++) {
             String string = stack.getDescriptionId() + "." + Nitrogen.MODID + ".ability.tooltip." + i;
             if (I18n.exists(string)) {
                 Component component = Component.translatable(string);
                 if (PREDICATES.containsKey(stack.getItemHolder())) {
-                    component = PREDICATES.get(stack.getItemHolder()).override(stack, components, component);
+                    component = PREDICATES.get(stack.getItemHolder()).override(player, stack, components, context, component);
                 }
                 components.add(i, component);
             }
@@ -40,6 +43,6 @@ public class TooltipListeners {
 
     @FunctionalInterface
     public interface TooltipPredicate {
-        Component override(ItemStack itemStack, List<Component> components, Component component);
+        Component override(Player player, ItemStack stack, List<Component> components, Item.TooltipContext context, Component component);
     }
 }
