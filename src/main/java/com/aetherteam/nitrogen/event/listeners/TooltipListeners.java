18e521c4ec21f746da0ae077393bc8363a2b68c9
bconlon <bconlon.gml@gmail.com>
2024-09-29 19:48:53 -0700

    refactor: loop to check for item supplier instead

--- src/main/java/com/aetherteam/nitrogen/event/listeners/TooltipListeners.java
+++ src/main/java/com/aetherteam/nitrogen/event/listeners/TooltipListeners.java
@@ -2,10 +2,9 @@ package com.aetherteam.nitrogen.event.listeners;
 
 import com.aetherteam.nitrogen.Nitrogen;
 import net.minecraft.client.resources.language.I18n;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,10 +13,11 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = Nitrogen.MODID)
 public class TooltipListeners {
-    public static Map<ResourceLocation, TooltipPredicate> PREDICATES = new HashMap<>();
+    public static Map<Supplier<Item>, TooltipPredicate> PREDICATES = new HashMap<>();
 
     @SubscribeEvent
     public static void onTooltipCreationLowPriority(ItemTooltipEvent event) {
@@ -28,13 +28,19 @@ public class TooltipListeners {
     }
 
     public static void addAbilityTooltips(Player player, ItemStack stack, List<Component> components) {
+        TooltipPredicate predicate = null;
+        for (Map.Entry<Supplier<Item>, TooltipPredicate> entry : PREDICATES.entrySet()) {
+            if (entry.getKey().get() == stack.getItem()) {
+                predicate = entry.getValue();
+                break;
+            }
+        }
         for (int i = 1; i <= 5; i++) {
             String string = stack.getDescriptionId() + "." + Nitrogen.MODID + ".ability.tooltip." + i;
             if (I18n.exists(string)) {
                 Component component = Component.translatable(string);
-                ResourceLocation location = BuiltInRegistries.ITEM.getKey(stack.getItem());
-                if (PREDICATES.containsKey(location)) {
-                    component = PREDICATES.get(location).override(player, stack, components, component);
+                if (predicate != null) {
+                    component = predicate.override(player, stack, components, component);
                 }
                 components.add(i, component);
             }
