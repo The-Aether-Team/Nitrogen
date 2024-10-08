260b395d7fbbb0ffb44c4a5a66538364600d9884
bconlon <bconlon.gml@gmail.com>
2024-10-07 18:54:45 -0700

    feat: restore JEI and REI compat

--- src/main/java/com/aetherteam/nitrogen/integration/jei/FluidStateRenderer.java
+++ src/main/java/com/aetherteam/nitrogen/integration/jei/FluidStateRenderer.java
@@ -1,108 +1,127 @@
 package com.aetherteam.nitrogen.integration.jei;
 
-//import com.aetherteam.nitrogen.Nitrogen;
-//import com.mojang.blaze3d.systems.RenderSystem;
-//import com.mojang.blaze3d.vertex.BufferBuilder;
-//import com.mojang.blaze3d.vertex.PoseStack;
-//import com.mojang.blaze3d.vertex.Tesselator;
-//import com.mojang.math.Axis;
-//import mezz.jei.api.ingredients.IIngredientRenderer;
-//import mezz.jei.api.ingredients.IIngredientTypeWithSubtypes;
-//import mezz.jei.common.platform.IPlatformFluidHelperInternal;
-//import net.minecraft.client.Minecraft;
-//import net.minecraft.client.gui.GuiGraphics;
-//import net.minecraft.client.renderer.ItemBlockRenderTypes;
-//import net.minecraft.client.renderer.RenderType;
-//import net.minecraft.client.renderer.block.BlockRenderDispatcher;
-//import net.minecraft.core.BlockPos;
-//import net.minecraft.network.chat.Component;
-//import net.minecraft.world.item.TooltipFlag;
-//import net.minecraft.world.level.block.Blocks;
-//import net.minecraft.world.level.block.state.BlockState;
-//import net.minecraft.world.level.material.Fluid;
-//import net.minecraft.world.level.material.FluidState;
-//import net.minecraft.world.level.material.Fluids;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public record FluidStateRenderer<T>(IPlatformFluidHelperInternal<T> fluidHelper) implements IIngredientRenderer<T> {
-//    @Override
-//    public void render(GuiGraphics guiGraphics, T ingredient) {
-//        PoseStack poseStack = guiGraphics.pose();
-//        Minecraft minecraft = Minecraft.getInstance();
-//        BlockRenderDispatcher blockRenderDispatcher = minecraft.getBlockRenderer();
-//
-//        poseStack.pushPose();
-//
-//        poseStack.translate(15.0F, 12.33F, 0.0F);
-//        poseStack.mulPose(Axis.XP.rotationDegrees(-30.0F));
-//        poseStack.mulPose(Axis.YP.rotationDegrees(45.0F));
-//        poseStack.scale(-9.9F, -9.9F, -9.9F);
-//
-//        IIngredientTypeWithSubtypes<Fluid, T> type = this.fluidHelper.getFluidIngredientType();
-//        Fluid fluidType = type.getBase(ingredient);
-//        FluidState fluidState = fluidType.defaultFluidState();
-//        RenderType renderType = ItemBlockRenderTypes.getRenderLayer(fluidState);
-//        PoseStack worldStack = RenderSystem.getModelViewStack();
-//
-//        renderType.setupRenderState();
-//        worldStack.pushPose();
-//        worldStack.mulPoseMatrix(poseStack.last().pose());
-//        RenderSystem.applyModelViewMatrix();
-//
-//        Tesselator tesselator = Tesselator.getInstance();
-//        BufferBuilder builder = tesselator.getBuilder();
-//        builder.begin(renderType.mode(), renderType.format());
-//        blockRenderDispatcher.renderLiquid(BlockPos.ZERO, new FakeFluidLevel(fluidState), builder, fluidState.createLegacyBlock(), fluidState);
-//        if (builder.building()) {
-//            tesselator.end();
-//        }
-//
-//        renderType.clearRenderState();
-//        worldStack.popPose();
-//        RenderSystem.applyModelViewMatrix();
-//
-//        poseStack.popPose();
-//    }
-//
-//    @Override
-//    public List<Component> getTooltip(T ingredient, TooltipFlag tooltipFlag) {
-//        try {
-//            return this.fluidHelper.getTooltip(ingredient, tooltipFlag);
-//        } catch (RuntimeException | LinkageError e) {
-//            Component displayName = this.fluidHelper.getDisplayName(ingredient);
-//            Nitrogen.LOGGER.error("Failed to get tooltip for fluid: " + displayName, e);
-//            return new ArrayList<>();
-//        }
-//    }
-//
-//    /**
-//     * A fake level used for rendering.
-//     */
-//    private static class FakeFluidLevel extends FakeLevel {
-//        private final FluidState fluidState;
-//
-//        public FakeFluidLevel(FluidState fluidState) {
-//            this.fluidState = fluidState;
-//        }
-//
-//        @Override
-//        public BlockState getBlockState(BlockPos pos) {
-//            if (pos.equals(BlockPos.ZERO)) {
-//                return this.fluidState.createLegacyBlock();
-//            } else {
-//                return Blocks.AIR.defaultBlockState();
-//            }
-//        }
-//
-//        @Override
-//        public FluidState getFluidState(BlockPos pos) {
-//            if (pos.equals(BlockPos.ZERO)) {
-//                return this.fluidState;
-//            } else {
-//                return Fluids.EMPTY.defaultFluidState();
-//            }
-//        }
-//    }
-//}
+import com.aetherteam.nitrogen.Nitrogen;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.math.Axis;
+import mezz.jei.api.gui.builder.ITooltipBuilder;
+import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.IIngredientTypeWithSubtypes;
+import mezz.jei.common.platform.IPlatformFluidHelperInternal;
+import mezz.jei.common.util.RegistryUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.fluids.FluidStack;
+import org.joml.Matrix4fStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record FluidStateRenderer<T>(IPlatformFluidHelperInternal<T> fluidHelper) implements IIngredientRenderer<T> {
+    @Override
+    public void render(GuiGraphics guiGraphics, T ingredient) {
+        PoseStack poseStack = guiGraphics.pose();
+        Minecraft minecraft = Minecraft.getInstance();
+        BlockRenderDispatcher blockRenderDispatcher = minecraft.getBlockRenderer();
+
+        poseStack.pushPose();
+
+        poseStack.translate(15.0F, 12.33F, 0.0F);
+        poseStack.mulPose(Axis.XP.rotationDegrees(-30.0F));
+        poseStack.mulPose(Axis.YP.rotationDegrees(45.0F));
+        poseStack.scale(-9.9F, -9.9F, -9.9F);
+
+        IIngredientTypeWithSubtypes<Fluid, T> type = this.fluidHelper.getFluidIngredientType();
+        Fluid fluidType = type.getBase(ingredient);
+        FluidState fluidState = fluidType.defaultFluidState();
+        RenderType renderType = ItemBlockRenderTypes.getRenderLayer(fluidState);
+        Matrix4fStack worldStack = RenderSystem.getModelViewStack();
+
+        renderType.setupRenderState();
+        worldStack.pushMatrix().mul(poseStack.last().pose());
+        RenderSystem.applyModelViewMatrix();
+
+        Tesselator tesselator = Tesselator.getInstance();
+        BufferBuilder builder = tesselator.begin(renderType.mode(), renderType.format());
+        blockRenderDispatcher.renderLiquid(BlockPos.ZERO, FakeLevel.of(fluidState), builder, fluidState.createLegacyBlock(), fluidState);
+
+        renderType.clearRenderState();
+        worldStack.popMatrix();
+        RenderSystem.applyModelViewMatrix();
+
+        poseStack.popPose();
+    }
+
+    @Override
+    public List<Component> getTooltip(T ingredient, TooltipFlag tooltipFlag) {
+        List<Component> components = new ArrayList<>();
+        if (ingredient instanceof FluidStack fluidStack) {
+            Fluid fluid = fluidStack.getFluid();
+            if (fluid.isSame(Fluids.EMPTY)) {
+                return List.of();
+            }
+
+            Component displayName = this.fluidHelper().getDisplayName(ingredient);
+            components.add(displayName);
+
+            if (tooltipFlag.isAdvanced()) {
+                Registry<Fluid> fluidRegistry = RegistryUtil.getRegistry(Registries.FLUID);
+                ResourceLocation resourceLocation = fluidRegistry.getKey(fluid);
+                if (resourceLocation != null && resourceLocation != BuiltInRegistries.FLUID.getDefaultKey()) {
+                    MutableComponent advancedId = Component.literal(resourceLocation.toString())
+                        .withStyle(ChatFormatting.DARK_GRAY);
+                    components.add(advancedId);
+                }
+            }
+        }
+        return components;
+    }
+
+    /**
+     * A fake level used for rendering.
+     */
+    private static class FakeFluidLevel extends FakeLevel {
+        private final FluidState fluidState;
+
+        public FakeFluidLevel(FluidState fluidState) {
+            this.fluidState = fluidState;
+        }
+
+        @Override
+        public BlockState getBlockState(BlockPos pos) {
+            if (pos.equals(BlockPos.ZERO)) {
+                return this.fluidState.createLegacyBlock();
+            } else {
+                return Blocks.AIR.defaultBlockState();
+            }
+        }
+
+        @Override
+        public FluidState getFluidState(BlockPos pos) {
+            if (pos.equals(BlockPos.ZERO)) {
+                return this.fluidState;
+            } else {
+                return Fluids.EMPTY.defaultFluidState();
+            }
+        }
+    }
+}
--- src/main/java/com/aetherteam/nitrogen/integration/rei/categories/AbstractRecipeCategory.java
+++ src/main/java/com/aetherteam/nitrogen/integration/rei/categories/AbstractRecipeCategory.java
@@ -1,64 +1,64 @@
 package com.aetherteam.nitrogen.integration.rei.categories;
 
-//import me.shedaniel.math.Point;
-//import me.shedaniel.math.Rectangle;
-//import me.shedaniel.rei.api.client.gui.Renderer;
-//import me.shedaniel.rei.api.client.gui.widgets.Widget;
-//import me.shedaniel.rei.api.client.gui.widgets.Widgets;
-//import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
-//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-//import me.shedaniel.rei.api.common.display.Display;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public abstract class AbstractRecipeCategory<T extends Display> implements DisplayCategory<T> {
-//    protected final String id;
-//    protected final CategoryIdentifier<T> uid;
-//
-//    protected final int width;
-//    protected final int height;
-//
-//    protected final int padding = 5;
-//
-//    protected final Renderer icon;
-//
-//    public AbstractRecipeCategory(String id, CategoryIdentifier<T> uid, int width, int height, Renderer icon) {
-//        this.id = id;
-//        this.uid = uid;
-//        this.width = width;
-//        this.height = height;
-//        this.icon = icon;
-//    }
-//
-//    @Override
-//    public CategoryIdentifier<T> getCategoryIdentifier() {
-//        return this.uid;
-//    }
-//
-//    @Override
-//    public Renderer getIcon() {
-//        return this.icon;
-//    }
-//
-//    @Override
-//    public List<Widget> setupDisplay(T display, Rectangle bounds) {
-//        List<Widget> widgets = new ArrayList<>();
-//        widgets.add(Widgets.createRecipeBase(new Rectangle(bounds.x, bounds.y, getDisplayWidth(display), getDisplayHeight())));
-//        return widgets;
-//    }
-//
-//    @Override
-//    public int getDisplayHeight() {
-//        return this.height + (this.padding * 2);
-//    }
-//
-//    @Override
-//    public int getDisplayWidth(T display) {
-//        return this.width + (this.padding * 2);
-//    }
-//
-//    protected Point startingOffset(Rectangle bounds) {
-//        return new Point(bounds.x + this.padding, bounds.y + this.padding);
-//    }
-//}
+import me.shedaniel.math.Point;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.gui.Renderer;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.Display;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractRecipeCategory<T extends Display> implements DisplayCategory<T> {
+    protected final String id;
+    protected final CategoryIdentifier<T> uid;
+
+    protected final int width;
+    protected final int height;
+
+    protected final int padding = 5;
+
+    protected final Renderer icon;
+
+    public AbstractRecipeCategory(String id, CategoryIdentifier<T> uid, int width, int height, Renderer icon) {
+        this.id = id;
+        this.uid = uid;
+        this.width = width;
+        this.height = height;
+        this.icon = icon;
+    }
+
+    @Override
+    public CategoryIdentifier<T> getCategoryIdentifier() {
+        return this.uid;
+    }
+
+    @Override
+    public Renderer getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public List<Widget> setupDisplay(T display, Rectangle bounds) {
+        List<Widget> widgets = new ArrayList<>();
+        widgets.add(Widgets.createRecipeBase(new Rectangle(bounds.x, bounds.y, getDisplayWidth(display), getDisplayHeight())));
+        return widgets;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return this.height + (this.padding * 2);
+    }
+
+    @Override
+    public int getDisplayWidth(T display) {
+        return this.width + (this.padding * 2);
+    }
+
+    protected Point startingOffset(Rectangle bounds) {
+        return new Point(bounds.x + this.padding, bounds.y + this.padding);
+    }
+}
